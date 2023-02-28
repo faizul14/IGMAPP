f432b7da12f7ac49cf857ba596015ce1a573d1da
Faezol <fpcyber61@gmail.com>
2023-02-28 17:14:22 +0800

    + Add Unit test on Use Case

--- app/src/test/java/com/informasigempabumi/igmapp/core/domain/usecase/UseCaseIteractorTest.kt
+++ app/src/test/java/com/informasigempabumi/igmapp/core/domain/usecase/UseCaseIteractorTest.kt
@@ -3,12 +3,9 @@ package com.informasigempabumi.igmapp.core.domain.usecase
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.informasigempabumi.igmapp.core.data.GetDataGempaRepository
 import com.informasigempabumi.igmapp.core.domain.model.DataGempa
 import com.informasigempabumi.igmapp.core.domain.repository.IGetDataGempaRespository
 import com.informasigempabumi.igmapp.ui.DataDummy
-import com.informasigempabumi.igmapp.ui.home.HomeViewModel
-import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -19,7 +16,7 @@ import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class UseCaseIteractorTest{
+class UseCaseIteractorTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
@@ -45,9 +42,62 @@ class UseCaseIteractorTest{
             val actualDataGempa = useCaseIter.getGempaTerbaru().observeForever(observer)
 
             Mockito.verify(repository).getGempaTerbaru()
-            Assert.assertNotNull(actualDataGempa)
+            assertNotNull(actualDataGempa)
         } finally {
             useCaseIter.getGempaTerbaru().removeObserver(observer)
         }
     }
+
+    @Test
+    fun `when get useCaseGetDataGempaTerkini Should Not Null`() {
+        val observer = Observer<List<DataGempa>> {}
+        try {
+            val ecpectedDataGempa = MutableLiveData<List<DataGempa>>()
+            ecpectedDataGempa.value = dataDummyList
+            Mockito.`when`(repository.getGempaTerkini()).thenReturn(ecpectedDataGempa)
+
+            val actualDataGempa = useCaseIter.getGempaTerkini().observeForever(observer)
+
+            Mockito.verify(repository).getGempaTerkini()
+            assertNotNull(actualDataGempa)
+        } finally {
+            useCaseIter.getGempaTerkini().removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun `when get useCaseGetDataGempaDiRasakan Should Not Null`() {
+        val observer = Observer<List<DataGempa>> {}
+        try {
+            val ecpectedDataGempa = MutableLiveData<List<DataGempa>>()
+            ecpectedDataGempa.value = dataDummyList
+            Mockito.`when`(repository.getGempaDiRasakan()).thenReturn(ecpectedDataGempa)
+
+            val actualDataGempa = useCaseIter.getGempaDiRasakan().observeForever(observer)
+
+            Mockito.verify(repository).getGempaDiRasakan()
+            assertNotNull(actualDataGempa)
+        } finally {
+            useCaseIter.getGempaDiRasakan().removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun `when get useCaseGetDataGempaCombine Should Not Null`() {
+        val observer = Observer<List<DataGempa>> {}
+        try {
+            val ecpectedDataGempa = MutableLiveData<List<DataGempa>>()
+            ecpectedDataGempa.value = dataDummyList
+            Mockito.`when`(repository.getDataCombine()).thenReturn(ecpectedDataGempa)
+
+            val actualDataGempa = useCaseIter.getDataCombine().observeForever(observer)
+
+            Mockito.verify(repository).getDataCombine()
+            assertNotNull(actualDataGempa)
+        } finally {
+            useCaseIter.getDataCombine().removeObserver(observer)
+        }
+    }
+
+
 }
