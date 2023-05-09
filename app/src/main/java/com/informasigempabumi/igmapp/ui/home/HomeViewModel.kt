33783d393b5eb4bba2e27408bc9bcdb17b63dd95
Faezol <fpcyber61@gmail.com>
2023-05-09 20:04:30 +0800

    ! save change don't know

--- app/src/main/java/com/informasigempabumi/igmapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/informasigempabumi/igmapp/ui/home/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.informasigempabumi.igmapp.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.informasigempabumi.igmapp.core.domain.usecase.UseCase
 
